0b59c0b8f661a7ed057b20538d7f1465bc0eb66a
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-04-06 12:19:52 -0600

    primer commit

---
+++ src/tarea_labo2/Tarea_Labo2.java
@@ -0,0 +1,16 @@
+package tarea_labo2;
+
+/**
+ *
+ * @author Oscar Lovato
+ */
+public class Tarea_Labo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
