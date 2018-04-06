a3668bbab3843e20f121ce8dc8d5ed05519d2a41
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-04-06 12:22:31 -0600

    primer commit

---
+++ src/Producto.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Oscar Lovato
+ */
+public class Producto {
+    private String nombre;
+    Proveedor proveedor;
+    private int cantidad;
+    private double precio;
+    public Producto(String nombre, Proveedor proveedor,int cantidad, double precio){
+        this.nombre=nombre;
+        this.proveedor=proveedor;
+        this.cantidad=cantidad;
+        this.precio=precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
